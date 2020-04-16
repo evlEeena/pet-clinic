35426a7929af1de719e9a069171ebb491b7ef4cf
Elena Zaets <evleeena@gmail.com>
2020-04-16 18:03:59 +0300

    Added Vets list

--- pet-clinic-web/src/main/java/com/evleeena/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/evleeena/petclinic/controllers/VetController.java
@@ -1,14 +1,22 @@
 package com.evleeena.petclinic.controllers;
 
+import com.evleeena.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    @Resource
+    private VetService vetService;
+
     @RequestMapping({"", "/"})
-    public String vetList() {
+    public String vetList(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/list";
     }
 }

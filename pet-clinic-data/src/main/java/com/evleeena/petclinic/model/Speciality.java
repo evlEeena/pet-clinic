213838c85abc8e877abdadde9f74c8d51462469b
Elena Zaets <evleeena@gmail.com>
2020-05-08 16:51:58 +0300

    added Speciality and link it to vet

---
+++ pet-clinic-data/src/main/java/com/evleeena/petclinic/model/Speciality.java
@@ -0,0 +1,20 @@
+package com.evleeena.petclinic.model;
+
+public class Speciality {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Speciality{" +
+                "description='" + description + '\'' +
+                '}';
+    }
+}

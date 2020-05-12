5ffcaeb1835e3ac93f8e7d103e4957bc9c1d25f4
Elena Zaets <evleeena@gmail.com>
2020-05-12 17:00:30 +0300

    added PetType to bootstrap

--- pet-clinic-web/src/main/java/com/evleeena/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/evleeena/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.evleeena.petclinic.bootstrap;
 
 import com.evleeena.petclinic.model.Owner;
+import com.evleeena.petclinic.model.PetType;
 import com.evleeena.petclinic.model.Vet;
 import com.evleeena.petclinic.services.OwnerService;
+import com.evleeena.petclinic.services.PetTypeService;
 import com.evleeena.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -16,9 +18,22 @@ public class DataLoader implements CommandLineRunner {
     private OwnerService ownerService;
     @Resource
     private VetService vetService;
+    @Resource
+    private PetTypeService petTypeService;
 
     @Override
     public void run(String... args) {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Pet types: " + savedDogPetType + ", " + savedCatPetType);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");

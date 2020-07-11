0ca10aae0c5cd0257d350eac89ca9378a15db777
evleeena <evleeena@gmail.com>
2020-07-11 08:37:07 +0300

    updated owners, pet and petType with jpa entity

--- pet-clinic-data/src/main/java/com/evleeena/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/evleeena/petclinic/model/PetType.java
@@ -1,6 +1,14 @@
 package com.evleeena.petclinic.model;
 
-public class PetType extends Pet {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
+public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {

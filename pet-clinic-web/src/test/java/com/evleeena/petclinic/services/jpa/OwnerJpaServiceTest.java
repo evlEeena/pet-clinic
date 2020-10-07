dc21b2c0992d9a387e95c2b8079319bad26c1223
evleeena <evleeena@gmail.com>
2020-10-07 21:12:52 +0300

    fix circle ci config

--- pet-clinic-web/src/test/java/com/evleeena/petclinic/services/jpa/OwnerJpaServiceTest.java
+++ pet-clinic-web/src/test/java/com/evleeena/petclinic/services/jpa/OwnerJpaServiceTest.java
@@ -19,6 +19,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+// to check build
 @ExtendWith(MockitoExtension.class)
 class OwnerJpaServiceTest {
 
